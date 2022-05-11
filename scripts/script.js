d00afcfa719aaa354513d33969e9598c48736715
fsvboas <felippesantosvb@hotmail.com>
2022-05-11 13:18:48 -0300

    Adjust responsiveness

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,17 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    scroll: true,
-    responsive:{
-        0:{
-            items: 1
-        },
-        600:{
-            items: 1
-        },
-        1000:{
-            items: 1
-        }
-    }
-})
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: false,
+  scroll: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+  },
+});
